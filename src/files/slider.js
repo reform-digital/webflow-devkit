3e1ff6d1e53e442eb1bd16169d044f0534f2cee2
reform-digital <hello@reform.digital>
2023-12-29 12:55:53 +0200

    Added Slider.js template code

--- src/files/slider.js
+++ src/files/slider.js
@@ -1,48 +1,47 @@
-import Swiper from "swiper";
-import { Navigation, Pagination, Scrollbar, Keyboard } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+// import Swiper from "swiper";
+// import { Navigation, Pagination, Scrollbar, Keyboard } from "swiper/modules";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+// import "swiper/css/scrollbar";
 
-document
-  .querySelectorAll(".slider-main_component")
-  .forEach((sliderComponent) => {
-    new Swiper(sliderComponent.querySelector(".swiper"), {
-      // Install modules
-      modules: [Navigation, Pagination, Scrollbar, Keyboard],
-      speed: 300,
-      loop: false,
-      rewind: true,
-      autoHeight: false,
-      centeredSlides: false,
-      followFinger: true,
-      freeMode: true,
-      slideToClickedSlide: false,
-      slidesPerView: "auto",
-      keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-      },
-      pagination: {
-        el: sliderComponent.querySelector(".swiper-bullet-wrapper"),
-        bulletActiveClass: "is-active",
-        bulletClass: "swiper-bullet",
-        bulletElement: "button",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: sliderComponent.querySelector(".swiper-next"),
-        prevEl: sliderComponent.querySelector(".swiper-prev"),
-        disabledClass: "is-disabled",
-      },
-      scrollbar: {
-        el: sliderComponent.querySelector(".swiper-drag-wrapper"),
-        draggable: true,
-        dragClass: "swiper-drag",
-        snapOnRelease: false,
-      },
-      slideActiveClass: "is-active",
-      slideDuplicateActiveClass: "is-active",
-    });
-  });
+// document
+//   .querySelectorAll(".slider-main_component")
+//   .forEach((sliderComponent) => {
+//     new Swiper(sliderComponent.querySelector(".swiper"), {
+//       modules: [Navigation, Pagination, Scrollbar, Keyboard],
+//       speed: 300,
+//       loop: false,
+//       rewind: true,
+//       autoHeight: false,
+//       centeredSlides: false,
+//       followFinger: true,
+//       freeMode: true,
+//       slideToClickedSlide: false,
+//       slidesPerView: "auto",
+//       keyboard: {
+//         enabled: true,
+//         onlyInViewport: true,
+//       },
+//       pagination: {
+//         el: sliderComponent.querySelector(".swiper-bullet-wrapper"),
+//         bulletActiveClass: "is-active",
+//         bulletClass: "swiper-bullet",
+//         bulletElement: "button",
+//         clickable: true,
+//       },
+//       navigation: {
+//         nextEl: sliderComponent.querySelector(".swiper-next"),
+//         prevEl: sliderComponent.querySelector(".swiper-prev"),
+//         disabledClass: "is-disabled",
+//       },
+//       scrollbar: {
+//         el: sliderComponent.querySelector(".swiper-drag-wrapper"),
+//         draggable: true,
+//         dragClass: "swiper-drag",
+//         snapOnRelease: false,
+//       },
+//       slideActiveClass: "is-active",
+//       slideDuplicateActiveClass: "is-active",
+//     });
+//   });
