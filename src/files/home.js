9c1be5f4c8a5531675ff3064fc55b85e74b2572f
reform-digital <hello@reform.digital>
2023-12-29 12:56:37 +0200

    Added Webflow ready code

--- src/files/home.js
+++ src/files/home.js
@@ -1,5 +1,8 @@
 import { greet } from "../modules/greet.js";
 import { checkScriptLoaded } from "../modules/scriptLogger.js";
 
-checkScriptLoaded("Home");
-greet("Mike");
+window.Webflow = window.Webflow || [];
+window.Webflow.push(function () {
+    checkScriptLoaded("Home");
+    greet("Mike");
+});
