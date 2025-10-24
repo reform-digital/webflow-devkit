2cdd67e3fd09bb238b70188d7aa9a24ca5af9d35
Mike Zachariades <mike@reform.digital>
2025-10-25 00:29:19 +0300

    Updated package versions

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
